f24955546edc9dda9788bdf17bb64c0295af0b78
seishiro.yamamoto <yamamotoseishiro@gmail.com>
2020-12-09 08:40:01 +0000

    likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likings, :likeds]
   def index
     @users=User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -38,6 +38,17 @@ class UsersController < ApplicationController
     counts(@user)
   end
   
+  def likes
+    @user = User.find(params[:id])
+    @likings = @user.likes.page(params[:page])
+    counts(@user)
+  end
+  
+  def likeds
+    @micropost = Micropost.find(params[:id])
+    @likeds = @micropost.likeds.page(params[:page])
+    counts(@micropost)
+  end
   private
   
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
-  get 'microposts/create'
-  get 'microposts/destroy'
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
@@ -18,9 +14,11 @@ Rails.application.routes.draw do
     member do
       get :followings
       get :followers
+      get :likes
     end
   end
 
-  resources :microposts ,only: [:create, :destroy]  
+  resources :microposts ,only: [:create, :destroy] 
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
